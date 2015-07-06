8c28017336000cbe057577fe343b864004f0b890
Toon Ketels <toon.ketels@gmail.com>
2015-07-06 10:42:51 +0200

    StateT.

--- Transformers.hs
+++ Transformers.hs
@@ -130,3 +130,74 @@ eval3 (App e1 e2)  = do val1 <- eval3 e1
                            _                  -> throwError "type error in application"
 
 -- runEval3 Map.empty (eval3 exampleExp)
+
+
+type Eval4 a = ReaderT Env (ErrorT String (StateT Integer Identity)) a
+
+runEval4 :: Env -> Integer -> Eval4 a -> (Either String a, Integer)
+runEval4 env state ev = runIdentity (runStateT (runErrorT (runReaderT ev env)) state)
+
+-- Helper to update the state with one
+tick :: (Num s, MonadState s m) => m ()
+tick = do state <- get
+          put (state + 1)
+
+eval4             :: Exp -> Eval4 Value
+eval4 (Lit i)      = do tick
+                        return $ IntVal i
+eval4 (Var n)      = do tick
+                        env <- ask
+                        case Map.lookup n env of
+                           Just val  -> return val
+                           Nothing   -> throwError ("unbound variable: " ++ n)
+eval4 (Plus e1 e2) = do tick
+                        e1' <- eval4 e1
+                        e2' <- eval4 e2
+                        case (e1', e2') of
+                            (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                            _                      -> throwError "type error in addition"
+eval4 (Abs n e)    = do tick
+                        env <- ask
+                        return $ FunVal env n e
+eval4 (App e1 e2)  = do tick
+                        val1 <- eval4 e1
+                        val2 <- eval4 e2
+                        case val1 of
+                           FunVal env' n body -> local (const (Map.insert n val2 env')) (eval4 body)
+                           _                  -> throwError "type error in application"
+
+
+-- runEval4 Map.empty 0 (eval4 exampleExp)
+
+type Eval4' a = ReaderT Env (StateT Integer (ErrorT String Identity)) a
+
+runEval4' :: Env -> Integer -> Eval4' a -> (Either String (a, Integer))
+runEval4' env state ev = runIdentity (runErrorT (runStateT (runReaderT ev env) state))
+
+eval4'             :: Exp -> Eval4' Value
+eval4' (Lit i)      = do tick
+                         return $ IntVal i
+eval4' (Var n)      = do tick
+                         env <- ask
+                         case Map.lookup n env of
+                            Just val  -> return val
+                            Nothing   -> throwError ("unbound variable: " ++ n)
+eval4' (Plus e1 e2) = do tick
+                         e1' <- eval4' e1
+                         e2' <- eval4' e2
+                         case (e1', e2') of
+                             (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                             _                      -> throwError "type error in addition"
+eval4' (Abs n e)    = do tick
+                         env <- ask
+                         return $ FunVal env n e
+eval4' (App e1 e2)  = do tick
+                         val1 <- eval4' e1
+                         val2 <- eval4' e2
+                         case val1 of
+                            FunVal env' n body -> local (const (Map.insert n val2 env')) (eval4' body)
+                            _                  -> throwError "type error in application"
+
+
+-- Swapping error and state give different results (on error)
+-- runEval4' Map.empty 0 (eval4' exampleExp)

86b14cbe791c36e966d7ef3ae558fdfa43fb6af3
Toon Ketels <toon.ketels@gmail.com>
2015-07-06 09:40:23 +0200

    ErrorT.

--- Transformers.hs
+++ Transformers.hs
@@ -41,7 +41,7 @@ eval0 env (App e1 e2)  = let val1 = eval0 env e1
 -- 12 + ((\x -> x)(4 + 2))
 -- eval0 Map.empty exampleExp
 exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x"))(Lit 4 `Plus` Lit 2))
-
+invalidExp = Plus (Lit 1) (Abs "x" (Var "x"))
 
 type Eval1 a = Identity a
 
@@ -64,4 +64,42 @@ eval1 env (App e1 e2)  = do val1 <- eval1 env e1
 -- runEval1 $ eval1 Map.empty exampleExp    IntVal 18
 
 
+-- ErrorT error monad
+type Eval2 a = ErrorT String Identity a
 
+runEval2   :: Eval2 a -> Either String a
+runEval2 ev = runIdentity (runErrorT ev)
+
+
+eval2a                 :: Env -> Exp -> Eval2 Value
+eval2a env (Lit i)      = return $ IntVal i
+eval2a env (Var n)      = return $ fromJust $ Map.lookup n env
+eval2a env (Plus e1 e2) = do IntVal i1 <- eval2a env e1
+                             IntVal i2 <- eval2a env e2
+                             return $ IntVal (i1 + i2)
+eval2a env (Abs n e)    = return $ FunVal env n e
+eval2a env (App e1 e2)  = do val1 <- eval2a env e1
+                             val2 <- eval2a env e2
+                             case val1 of
+                               FunVal env' n body -> eval2a (Map.insert n val2 env') body
+
+-- eval2a Map.epmty exampleExp               ErrorT (Identity (Right (IntVal 18)))
+-- runEval2 $ eval2a Map.empty exampleExp    Right (IntVal 18)
+
+-- better errors
+eval2b                 :: Env -> Exp -> Eval2 Value
+eval2b env (Lit i)      = return $ IntVal i
+eval2b env (Var n)      = case Map.lookup n env of
+                            Just val  -> return val
+                            Nothing   -> throwError ("unbound variable: " ++ n)
+eval2b env (Plus e1 e2) = do e1' <- eval2b env e1
+                             e2' <- eval2b env e2
+                             case (e1', e2') of
+                                (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                                _                      -> throwError "type error in addition"
+eval2b env (Abs n e)    = return $ FunVal env n e
+eval2b env (App e1 e2)  = do val1 <- eval2b env e1
+                             val2 <- eval2b env e2
+                             case val1 of
+                               FunVal env' n body -> eval2b (Map.insert n val2 env') body
+                               _                  -> throwError "type error in application"

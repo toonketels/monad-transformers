3bd01105fd343c384cf5c2c90b0c94939cb49fea
Toon Ketels <toon.ketels@gmail.com>
2015-07-06 10:58:44 +0200

    WriterT.

--- Transformers.hs
+++ Transformers.hs
@@ -201,3 +201,38 @@ eval4' (App e1 e2)  = do tick
 
 -- Swapping error and state give different results (on error)
 -- runEval4' Map.empty 0 (eval4' exampleExp)
+
+
+type Eval5 a = ReaderT Env (ErrorT String
+                           (WriterT [String] (StateT Integer Identity))) a
+
+runEval5             :: Env -> Integer -> Eval5 a -> ((Either String a, [String]), Integer)
+runEval5 env state ev = runIdentity (runStateT (runWriterT (runErrorT (runReaderT ev env))) state)
+
+
+eval5             :: Exp -> Eval5 Value
+eval5 (Lit i)      = do tick
+                        return $ IntVal i
+eval5 (Var n)      = do tick
+                        tell [n]
+                        env <- ask
+                        case Map.lookup n env of
+                           Just val  -> return val
+                           Nothing   -> throwError ("unbound variable: " ++ n)
+eval5 (Plus e1 e2) = do tick
+                        e1' <- eval5 e1
+                        e2' <- eval5 e2
+                        case (e1', e2') of
+                            (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                            _                      -> throwError "type error in addition"
+eval5 (Abs n e)    = do tick
+                        env <- ask
+                        return $ FunVal env n e
+eval5 (App e1 e2)  = do tick
+                        val1 <- eval5 e1
+                        val2 <- eval5 e2
+                        case val1 of
+                           FunVal env' n body -> local (const (Map.insert n val2 env')) (eval5 body)
+                           _                  -> throwError "type error in application"
+
+-- runEval5 Map.empty 0 (eval5 exampleExp)

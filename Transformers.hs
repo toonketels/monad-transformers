939b2f916126270247451ff4ce6b59b766649328
Toon Ketels <toon.ketels@gmail.com>
2015-07-06 08:56:12 +0200

    Monadic form.

--- Transformers.hs
+++ Transformers.hs
@@ -26,9 +26,9 @@ type Env = Map.Map Name Value   -- Maps names to values
 
 -- Reference implementation interpreter
 
-eval0 :: Env -> Exp -> Value
-eval0 env (Lit i) = IntVal i
-eval0 env (Var n) = fromJust (Map.lookup n env)
+eval0                 :: Env -> Exp -> Value
+eval0 env (Lit i)      = IntVal i
+eval0 env (Var n)      = fromJust (Map.lookup n env)
 eval0 env (Plus e1 e2) = let IntVal i1 = eval0 env e1
                              IntVal i2 = eval0 env e2
                          in IntVal (i1 + i2)
@@ -41,3 +41,27 @@ eval0 env (App e1 e2)  = let val1 = eval0 env e1
 -- 12 + ((\x -> x)(4 + 2))
 -- eval0 Map.empty exampleExp
 exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x"))(Lit 4 `Plus` Lit 2))
+
+
+type Eval1 a = Identity a
+
+runEval1   :: Eval1 a -> a
+runEval1 ev = runIdentity ev
+
+eval1                 :: Env -> Exp -> Eval1 Value
+eval1 env (Lit i)      = return $ IntVal i
+eval1 env (Var n)      = return $ fromJust $ Map.lookup n env
+eval1 env (Plus e1 e2) = do IntVal i1 <- eval1 env e1
+                            IntVal i2 <- eval1 env e2
+                            return $ IntVal (i1 + i2)
+eval1 env (Abs n e)    = return $ FunVal env n e
+eval1 env (App e1 e2)  = do val1 <- eval1 env e1
+                            val2 <- eval1 env e2
+                            case val1 of
+                              FunVal env' n body -> eval1 (Map.insert n val2 env') body
+
+-- eval1 Map.empty exampleExp               Identity (IntVal 18)
+-- runEval1 $ eval1 Map.empty exampleExp    IntVal 18
+
+
+
